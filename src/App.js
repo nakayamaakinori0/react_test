6cacfefe323cd8fef46e7030bde8e02d3d306ba4
akinori-nakayama <akinori.nakayama@play.jp>
2022-09-16 15:39:53 +0900

    update test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import Share from "./Share";
-import "./styles.css";
+// import Share from "./Share";
+// import "./styles.css";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <Share/>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Share/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
+
+export default App;
